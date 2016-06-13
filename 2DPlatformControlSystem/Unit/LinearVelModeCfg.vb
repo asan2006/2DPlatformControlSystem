4960fef67e7b8e8fb3c973272b032c4396ebf981
Jmpep <bin___03@163.com>
2016-06-13 15:39:49 +0800

    20160613 Linear, Rotate configure 模块搭建 更改gitignore文件，使包含SLN文件

--- 2DPlatformControlSystem/Unit/LinearVelModeCfg.vb
+++ 2DPlatformControlSystem/Unit/LinearVelModeCfg.vb
@@ -70,12 +70,5 @@
             chkLMode.Checked = value
         End Set
     End Property
-    Sub New()
-
-        ' 此调用是设计器所必需的。
-        InitializeComponent()
-
-        ' 在 InitializeComponent() 调用之后添加任何初始化。
-    End Sub
 
 End Class
