d4075d3dab084af8506bab0e9e313caee239b700
Hard-PC <bin___03@163.com>
2017-06-12 10:18:16 +0800

    V1.2 再次更改图标设计

--- 2DPlatformControlSystem/MainForm.Designer.vb
+++ 2DPlatformControlSystem/MainForm.Designer.vb
@@ -22,6 +22,7 @@ Partial Class MainForm
     '不要使用代码编辑器修改它。
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainForm))
         Me.TabControl1 = New System.Windows.Forms.TabControl()
         Me.tbTrdControl = New System.Windows.Forms.TabPage()
         Me.TrdControlPanel1 = New _2DPlatformControlSystem.TrdControlPanel()
@@ -175,6 +176,7 @@ Partial Class MainForm
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(928, 572)
         Me.Controls.Add(Me.TabControl1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "MainForm"
         Me.Text = "2DPlatformControlSystem"
         Me.TabControl1.ResumeLayout(False)
